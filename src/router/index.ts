0917ec7765fc1443896922e2fdd447848148d937
luocong2016 <luocong2016@126.com>
2022-03-25 10:20:56 +0800

    docs: test

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
+    path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue')
   }
